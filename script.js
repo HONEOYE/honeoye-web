607038e332008a89e8e709b94a0c8d21e39a0cc6
HONEOYE <125215678+HONEOYE@users.noreply.github.com>
2025-03-18 06:42:37 +0300

    Update script.js

--- script.js
+++ script.js
@@ -249,7 +249,7 @@ terminalInput.addEventListener('keypress', (e) => {
 // Музыкальный плеер
 const tracks = [
     { url: "https://drive.google.com/uc?export=download&id=1Y9qzwlbip3UTgNysyRr7qCBXpAGdyFf1", name: "mindvacy - outer heaven.mp3" },
-    { url: "https://drive.google.com/uc?export=download&id=1Y9gmieMff_TWd9g7bT4h5d-B46jPa5FS", name: "Sewerslvt - her.mp3" },
+    { url: "/music/track2.mp3", name: "Sewerslvt - her.mp3" },
     { url: "https://example.com/track3.mp3", name: "Wired Echo" }
     // Замени на свои ссылки (например, https://drive.google.com/uc?export=download&id=ИД_ФАЙЛА)
 ];
